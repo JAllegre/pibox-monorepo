efd6c1ad3497f672e70a6fed5d24291db798fa0e
JAllegre <ju.allegre@gmail.com>
2024-05-02 16:08:29 +0200

    deploy: remove useless 808x ports

--- checklist/vite.config.ts
+++ checklist/vite.config.ts
@@ -1,9 +1,8 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 const nodeEnv = process.env.NODE_ENV || "development";
-const env = loadEnv(nodeEnv, process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,8 +13,5 @@ export default defineConfig({
       "@src": path.resolve(__dirname, "./src"),
       "@common": path.resolve(__dirname, "../common"),
     },
-  },
-  server: {
-    port: parseInt(env.VITE_PORT),
   },
 });
--- miam/vite.config.ts
+++ miam/vite.config.ts
@@ -1,10 +1,8 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 const nodeEnv = process.env.NODE_ENV || "development";
-const env = loadEnv(nodeEnv, process.cwd());
-
 export default defineConfig({
   base: nodeEnv === "production" ? "/miam" : "/",
   plugins: [react()],
@@ -13,8 +11,5 @@ export default defineConfig({
       "@src": path.resolve(__dirname, "./src"),
       "@common": path.resolve(__dirname, "../common"),
     },
-  },
-  server: {
-    port: parseInt(env.VITE_PORT),
-  },
+  }
 });
