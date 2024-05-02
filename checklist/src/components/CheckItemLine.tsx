fdf7b52d3c95116eed87c0977a348949ebcabc21
JAllegre <ju.allegre@gmail.com>
2024-05-02 14:14:28 +0200

    checklist: Improve input validation

--- checklist/src/components/CheckItemLine.tsx
+++ checklist/src/components/CheckItemLine.tsx
@@ -1,13 +1,15 @@
-import { Box, Card, CardBody, Input, Stack, Switch } from "@chakra-ui/react";
+import { Box, Card, CardBody, Input, InputGroup, InputRightElement, Stack, Switch } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { ImCheckmark } from "react-icons/im";
 import { ChecklistItem, ChecklistItemInput, ChecklistItemStatus } from "../../../common/checklistTypes";
 import { DisplayMode } from "../types";
 import { useChecklistStore } from "../utils/ChecklistStore";
 import { updateItem } from "../utils/api";
 import eventMgr from "../utils/eventMgr";
 import { MyIconButton } from "./MyIconButton";
+
 interface CheckItemLineProps {
   checkItem: ChecklistItem;
   isNewItem?: boolean;
@@ -16,6 +18,8 @@ interface CheckItemLineProps {
 export default function CheckItemLine({ checkItem, isNewItem }: CheckItemLineProps) {
   const [title, setTitle] = useState(checkItem.title);
   const [isNew, setIsNew] = useState(isNewItem);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputEdited, setInputEdited] = useState(false);
   const [isItemChecked, setIsItemChecked] = useState(checkItem.checkStatus > ChecklistItemStatus.Unselected);
   const cardRef = useRef<HTMLDivElement>(null);
   const isEditMode = useChecklistStore((state) => state.displayMode === DisplayMode.Edit);
@@ -45,7 +49,6 @@ export default function CheckItemLine({ checkItem, isNewItem }: CheckItemLinePro
     // reset to the remote state when changed
     setIsItemChecked(checkItem.checkStatus > ChecklistItemStatus.Unselected);
   }, [checkItem.checkStatus]);
-
   const handleTitleChange = useCallback(
     async (event: ChangeEvent<HTMLInputElement>) => {
       setTitle(event.target.value);
@@ -54,6 +57,7 @@ export default function CheckItemLine({ checkItem, isNewItem }: CheckItemLinePro
   );
 
   const handleTitleBlur = useCallback(async () => {
+    setInputEdited(false);
     if (title === checkItem.title) {
       return;
     }
@@ -81,6 +85,14 @@ export default function CheckItemLine({ checkItem, isNewItem }: CheckItemLinePro
     setItemIdToDelete(checkItem.id);
   }, [setItemIdToDelete, checkItem.id]);
 
+  const handleInputDoubleClick = () => {
+    setInputEdited(true);
+  };
+
+  const handleValidationIconClick = () => {
+    inputRef.current?.blur();
+  };
+
   return (
     <Card ref={cardRef} my="1" bgColor={isNew ? "teal.700" : "gray.600"} color="teal.50">
       <CardBody px={2} py={1}>
@@ -88,17 +100,31 @@ export default function CheckItemLine({ checkItem, isNewItem }: CheckItemLinePro
           <Box>
             <Switch isChecked={isItemChecked} onChange={handleCheckSwitch} size="md" />
           </Box>
-          <Input
-            size="sm"
-            p={1}
-            placeholder="Entrez un nom"
-            value={title || ""}
-            onChange={handleTitleChange}
-            onBlur={handleTitleBlur}
-            readOnly={!isEditMode}
-            sx={{ border: !isEditMode ? "none" : "" }}
-            flexGrow={1}
-          />
+
+          <InputGroup>
+            <Input
+              ref={inputRef}
+              size="sm"
+              p={1}
+              placeholder="Entrez un nom"
+              defaultValue={checkItem.title || ""}
+              onChange={handleTitleChange}
+              onBlur={handleTitleBlur}
+              readOnly={!isEditMode || !inputEdited}
+              sx={{ border: !isEditMode ? "none" : "" }}
+              flexGrow={1}
+              onDoubleClick={handleInputDoubleClick}
+            />
+            <InputRightElement>
+              <MyIconButton
+                ReactIcon={ImCheckmark}
+                color="green.200"
+                onClick={handleValidationIconClick}
+                display={title !== checkItem.title ? "" : "none"}
+                mb={2}
+              />
+            </InputRightElement>
+          </InputGroup>
           <MyIconButton
             ReactIcon={FaRegTrashAlt}
             color="red.400"
