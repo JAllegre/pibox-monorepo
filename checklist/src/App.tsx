d46c3461dc568179e68a0453cddc9dd7ca211bab
JAllegre <ju.allegre@gmail.com>
2024-10-13 22:37:14 +0200

    checklist: add no cache on index

--- checklist/src/App.tsx
+++ checklist/src/App.tsx
@@ -1,11 +1,22 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
 import AppLayout from "./components/AppLayout";
 import theme from "./theme";
+import socketManager from "./utils/socketManager";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const isMountedRef = useRef(false);
+  useEffect(() => {
+    if (isMountedRef.current) {
+      return;
+    }
+    console.log("***ju***App.tsx/16", "");
+    isMountedRef.current = true;
+    socketManager.init();
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
--- checklist/src/main.tsx
+++ checklist/src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import socketManager from "./utils/socketManager";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-socketManager.init();
